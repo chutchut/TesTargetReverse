5f8dd5f6fdf433508850d521a28684f717533ad7
Calum Hutton <c.e.hutton@gmx.com>
2018-02-06 21:15:55 +0000

    Improvements to PoC code for more dynamic testing of arbitrary blocks

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string>
 #include "bignum.h"
+#include <array>
 
 static CBigNum bnProofOfWorkLimit(~uint256(0) >> 20);
 static CBigNum bnProofOfStakeLimit(~uint256(0) >> 24);
@@ -9,12 +10,16 @@ static CBigNum bnProofOfStakeHardLimit(~uint256(0) >> 30);
 using namespace::std;
 
 
-std::string padString(std::string in) {
+struct Block {
+    int array[5];
+};
+
+string padString(string in) {
 	int targetLen = 64;
 	int strLen = in.length();
 	int padNum = targetLen - strLen;
 
-	std:string outStr = "0x";
+	string outStr = "0x";
 	for (int i = 0; i < padNum; i++) {
 		outStr += "0";
 	}
@@ -23,15 +28,64 @@ std::string padString(std::string in) {
 	return outStr;
 }
 
-void getTarget(int index, int64 pBlockTime, int64 ppBlockTime, unsigned int prevBits, int isPos) {
+Block getBlock(int index, int blocks[][5]) {
+	for (int i = 0; i < sizeof(blocks); i++) {
+		if (blocks[i][0] == index) {
+			Block b;
+			b.array[0] = blocks[i][0];
+			b.array[1] = blocks[i][1];
+			b.array[2] = blocks[i][2];
+			b.array[3] = blocks[i][3];
+			b.array[4] = blocks[i][4];
+			return b;
+		}
+	}
+	// Null block
+	Block nb;
+	return nb;
+}
 
-	CBigNum maxTarget = bnProofOfWorkLimit; // Only changes if: (nHeight + 1 > 14060) or (nHeight + 1 > 15000)
+const int GetLastBlockIndex(int index, int blocks[][5], bool fProofOfStake) {
+	int pindex = index;
+	Block b = getBlock(pindex, blocks);
+    while (pindex > 0 && (pindex - 1) > 0 && ((b.array[4] == 1) != fProofOfStake))
+        pindex -= 1;
+    	b = getBlock(pindex, blocks);
+    return pindex;
+}
+
+void getTarget(int index, int blocks[][5]) {
+
+	// Get the block for the current index
+	Block block = getBlock(index, blocks);
+
+	// Previous bits for current block
+	unsigned int prevBits = block.array[3];
+
+	// Set pos/pow and target based on type
+	bool fProofOfStake = false;
+	CBigNum maxTarget = bnProofOfWorkLimit;
+	if (block.array[4] == 1) {
+		fProofOfStake = true;
+		if(index + 1 > 15000)
+			maxTarget = bnProofOfStakeLimit;
+		else if(index + 1 > 14060)
+			maxTarget = bnProofOfStakeHardLimit;
+	}
+
+	// Get indexes with GetLastBlockIndex()
+	int pBlockHeight = GetLastBlockIndex(index, blocks, fProofOfStake);
+	int ppBlockHeight = GetLastBlockIndex(pBlockHeight - 1, blocks, fProofOfStake);
+
+	// Get blocks for prev and prev-prev indexes
+	Block pblock = getBlock(pBlockHeight, blocks);
+	Block ppblock = getBlock(ppBlockHeight, blocks);
+
+	// Set timestamps from above blocks
+	int64 pBlockTime = pblock.array[1];
+	int64 ppBlockTime = ppblock.array[1];
 
 	// Reverse the algorithm
-	bool fProofOfStake = false;
-	if (isPos == 1) {
-		fProofOfStake = true;
-	}
 	unsigned int nStakeMinAge = 60 * 60 * 24; // minimum age for coin age, changed to 8 hours (24 hr realistic min) network will auto balance
 	unsigned int nStakeMaxAge = 60 * 60 * 24 * 90; // stake age of full weight
 	unsigned int nStakeTargetSpacing = 1 * 30; // 1-minute block spacing    --- actually is 30 second
@@ -50,15 +104,6 @@ void getTarget(int index, int64 pBlockTime, int64 ppBlockTime, unsigned int prev
 			<< "\nnTargetTimespan: " << nTargetTimespan
 			<< "\nnTargetSpacingWorkMax: " << nTargetSpacingWorkMax << "\n";
 
-	int pBlockHeight = index;
-	int ppBlockHeight = index;
-
-	/*
-	int64 pBlockTime = 1390010424;
-	int64 ppBlockTime = 1390010337;
-	unsigned int prevBits = 504365055;
-	*/
-
 	cout << "Inputs:\n";
 	cout << "pBlockHeight: " << pBlockHeight
 			<< "\nppBlockHeight: " << ppBlockHeight
@@ -69,7 +114,7 @@ void getTarget(int index, int64 pBlockTime, int64 ppBlockTime, unsigned int prev
 	cout << "Calculations:\n";
 	int64 actualSpacing = pBlockTime - ppBlockTime;
 	cout << "actualSpacing: " << actualSpacing << "\n";
-	int64 targetSpacing = fProofOfStake ? nStakeTargetSpacing : min(nTargetSpacingWorkMax, (int64) nStakeTargetSpacing * (1 + pBlockHeight - ppBlockHeight));
+	int64 targetSpacing = fProofOfStake ? nStakeTargetSpacing : min(nTargetSpacingWorkMax, (int64) nStakeTargetSpacing * (1 + index - pBlockHeight));
 	cout << "targetSpacing: " << targetSpacing << "\n";
 	int64 interval = nTargetTimespan / targetSpacing;
 	cout << "interval: " << interval << "\n";
@@ -78,11 +123,17 @@ void getTarget(int index, int64 pBlockTime, int64 ppBlockTime, unsigned int prev
 	CBigNum bnNew;
 	cout << "Setting bits in bignum\n";
 	bnNew.SetCompact(prevBits);
-	bnNew *= ((interval - 1) * targetSpacing + actualSpacing + actualSpacing);
+
+	// Right op 1
 	int64 rOp = ((interval - 1) * targetSpacing + actualSpacing + actualSpacing);
+	bnNew *= rOp;
 	cout << "Val after first op: " << bnNew.GetCompact() << " (" << padString(bnNew.GetHex()) << "). Right operand: " << rOp << "\n";
-	bnNew /= ((interval + 1) * targetSpacing);
+
+	// Right op 2
 	rOp = ((interval + 1) * targetSpacing);
+	bnNew /= rOp;
+
+	// New target
 	if (bnNew > maxTarget) {
 		cout << "New target (" << bnNew.GetCompact() << ") greater than max target! Setting to max target: " << maxTarget.GetCompact() << "\n";
 		bnNew = maxTarget;
@@ -114,8 +165,7 @@ int main(int argc, const char* argv[]) {
 	hexNum.SetHex(cmpHex);
 	cout << "GetCompact() from hex: " << padString(cmpHex) << ": " << hexNum.GetCompact() << "\n";
 
-	/*
-	int blocks[8][5] = {
+	int blocks1to8[8][5] = {
 	   {1, 1390009623, 1389962782, 504365055, 0},
 	   {2, 1390009701, 1390009623, 504365055, 0},
 	   {3, 1390009747, 1390009701, 504365055, 0},
@@ -126,13 +176,15 @@ int main(int argc, const char* argv[]) {
 	   {8, 1390010337, 1390010322, 504365055, 0}
 	};
 
-	for (int i = 0; i < 8; i++) {
-		cout << blocks[i][0] << " :: " << blocks[i][1] << " :: " << blocks[i][2] << " :: " << blocks[i][3] << " :: " << blocks[i][4] << "\n";
-		getTarget(blocks[i][0], blocks[i][1], blocks[i][2], blocks[i][3], blocks[i][4]);
+	for (int i = 0, j = 1; i < 8; i++, j++) {
+		cout << blocks1to8[i][0] << " :: " << blocks1to8[i][1] << " :: " << blocks1to8[i][2] << " :: " << blocks1to8[i][3] << " :: " << blocks1to8[i][4] << "\n";
+		getTarget(j, blocks1to8);
 	}
-	*/
 
 	// Block 9
-	getTarget(8, 1390010337, 1390010322, 504365055, 0);
+	//getTarget(8, 1390010337, 1390010322, 504365055, 0);
+
+	// Block 88535
+	//getTarget(88535, 1392636015, 1392635990, 486651519, 1);
 
 }
